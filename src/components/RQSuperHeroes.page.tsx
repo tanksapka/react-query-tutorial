bf6ed9e727611be2ac58a4e9b37b3b84bcaaaa04
Tamas Schell <tanksapka@yahoo.com>
2022-06-20 23:49:43 +0200

    Creating react query version of the page. Adding error handling and devtools.

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,5 +1,27 @@
+import axios, { AxiosResponse } from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => axios.get("http://localhost:4000/superheroes");
+
 function RQSuperHeroes() {
-  return <div>RQSuperHeroes</div>;
+  const { isLoading, isError, error, data } = useQuery<AxiosResponse, Error>("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQSuperHeroes</h2>
+      {data?.data.map((hero: { id: number; name: string; alterEgo: string }) => (
+        <div key={hero.id}>{hero.name}</div>
+      ))}
+    </>
+  );
 }
 
 export default RQSuperHeroes;
