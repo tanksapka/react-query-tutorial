d70230e45dea5f2ea96c03dbcca68ab00d25734b
Tamas Schell <tanksapka@yahoo.com>
2022-06-27 21:34:31 +0200

    Adding button to fetch data only on click as a demo. Homework of polling and exiting polling onSuccess and onError.

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,12 +1,40 @@
 import axios, { AxiosResponse } from "axios";
+import { useState } from "react";
 import { useQuery } from "react-query";
+
+interface SuperHero {
+  id: number;
+  name: string;
+  alterEgo: string;
+}
 
 const fetchSuperHeroes = () => axios.get("http://localhost:4000/superheroes");
 
 function RQSuperHeroes() {
-  const { isLoading, isError, error, data } = useQuery<AxiosResponse, Error>("super-heroes", fetchSuperHeroes);
+  const [interval, setInterval] = useState<number | false>(3000);
 
-  if (isLoading) {
+  const onSuccess = (data: AxiosResponse) => {
+    console.log("Perform side effect after data fetching", data);
+    if (data.data.length === 4) setInterval(false);
+  };
+
+  const onError = (data: Error) => {
+    console.log("Perform side effect after encountering error", data);
+    if (data.name) setInterval(false);
+  };
+
+  const { isLoading, isFetching, isError, error, data } = useQuery<AxiosResponse, Error>(
+    "super-heroes",
+    fetchSuperHeroes,
+    {
+      // enabled: false,
+      refetchInterval: interval,
+      onSuccess,
+      onError,
+    }
+  );
+
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
 
@@ -17,7 +45,8 @@ function RQSuperHeroes() {
   return (
     <>
       <h2>RQSuperHeroes</h2>
-      {data?.data.map((hero: { id: number; name: string; alterEgo: string }) => (
+      {/* <button onClick={() => refetch()}>Fetch heroes</button> */}
+      {data?.data.map((hero: SuperHero) => (
         <div key={hero.id}>{hero.name}</div>
       ))}
     </>
